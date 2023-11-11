4783c17df08ec8b1670bab941c5095e06b524b42
Syed Amaan <75304633+Ak-407@users.noreply.github.com>
2023-11-11 18:28:40 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # EZzon
+# EZzon
---
+++ app.js
@@ -0,0 +1,380 @@
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const md5 = require("md5");
+let multer = require("multer")
+const Razorpay = require('razorpay');
+
+
+
+const app = express();
+mongoose.set('strictQuery', true);
+// mongoose.connect("mongodb://localhost:27017/user3DB",{ useNewUrlParser: true , useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://jsamaan:amaan123@cluster0.vz55wc0.mongodb.net/jsamaan?retryWrites=true&w=majority",{ useNewUrlParser: true , useUnifiedTopology: true });
+let storage = multer.diskStorage({
+  destination:"public/photo/",
+  filename:(req,file,cb)=>{
+    // cb(null,Date.now(+file.originalname))
+    cb(null,file.originalname)
+  }
+})
+const upload = multer({ storage: storage }).single('file');
+
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+const File = new mongoose.Schema({
+  name:String,
+  email:String,
+  phone:Number,
+  productName:String,
+  productPrice:Number,
+  image:String
+});
+const Kitchen = new mongoose.Schema({
+  name:String,
+  email:String,
+  phone:Number,
+  productName:String,
+  productPrice:Number,
+  image:String
+});
+const Phone = new mongoose.Schema({
+  name:String,
+  email:String,
+  phone:Number,
+  productName:String,
+  productPrice:Number,
+  image:String
+});
+const Skill = new mongoose.Schema({
+  name:String,
+  profession:String,
+  experience:Number,
+  email:String,
+  phone:Number,
+  price:Number
+});
+const UserSchema = mongoose.Schema({
+  username:String,
+  password:String,
+});
+const Payment = new mongoose.Schema({
+  email:String,
+  name:String,
+  number:Number,
+  city:String,
+  address:String,
+  country:String,
+  pincode:Number
+});
+
+const razorpay = new Razorpay({
+  key_id: 'rzp_test_FCY7ZRu9Ix7mw5',
+  key_secret: 'SCdwoI1Vu7ngtCbd6jtZt94O',
+});
+
+
+const file = mongoose.model("file", File);
+const kitchen = mongoose.model("kitchen", Kitchen);
+const phone = mongoose.model("phone", Phone);
+const skill = mongoose.model("skill", Skill);
+const user = mongoose.model("user", UserSchema);
+const payment = mongoose.model("payment", Payment);
+
+// const cart = [];
+app.get("/", function(req, res) {
+  file.find({}, function(err, foundItems) {
+    kitchen.find({}, function(err, foundItemsk) {
+    phone.find({}, function(err, foundItemsp) {
+    skill.find({}, function(err, skillItems) {
+      res.render("header", { recordss: foundItems, skillName: skillItems, kitchen:foundItemsk, phone:foundItemsp });
+    });
+  })})});
+});
+
+app.post("/",function(req,res){
+  res.render("header")
+  search=req.body.search1
+  console.log(search)
+})
+app.get("/register", function(req,res){
+  res.render("register");
+});
+
+app.get("/login", function(req,res){
+  res.render("login");
+});
+
+app.get("/submit",function(req,res){
+  res.render("sec1");
+})
+app.get("/adminn",function(req,res){
+  res.render("adminn");
+  
+})
+app.get("/skill",function(req,res){
+  res.render("skill");
+  
+})
+app.get("/raz",function(req,res){
+  res.render("raz");
+})
+app.get("/select",function(req,res){
+  res.render("select");
+})
+app.get("/sports",function(req,res){
+  res.render("sports");
+})
+app.get("/kitchen",function(req,res){
+  res.render("kitchen");
+})
+app.get("/phone",function(req,res){
+  res.render("phone");
+})
+
+
+
+
+app.get("/payment", function(req, res) {
+      res.render("payment");
+    });
+app.post("/payment",function(req,res){
+  const paymentperson = new payment({
+    email: req.body.username,
+    name: req.body.name,
+    number:req.body.number,
+    city:req.body.city,
+    address: req.body.address,
+    country:req.body.country,
+    pincode:req.body.pincode
+  })
+  paymentperson.save(function(err, foundItems) {
+      if (!err) {
+          res.redirect("raz"); // Redirect to the home page
+      } else {
+          res.send(err);
+      }
+  });
+})
+
+
+
+
+
+app.post("/register", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Validate username and password
+  if (!username || !password) {
+    return res.status(400).send("Username and password are required.");
+  }
+
+  const newUser = new user({
+    username: username, // Make sure the username is mapped correctly
+    password: md5(password)
+  });
+
+  newUser.save(function(err) {
+    if (!err) {
+      res.render("select");
+    } else {
+      res.send(err);
+    }
+  });
+});
+app.post("/sports",upload,function(req,res){
+  const photo = new file({
+    name: req.body.name,
+    email: req.body.email,
+    phone:req.body.phone,
+    productName:req.body.productName,
+    productPrice:req.body.productPrice,
+    image: req.file.filename
+  })
+  photo.save(function(err, foundItems) {
+      if (!err) {
+          res.redirect("/"); // Redirect to the home page
+      } else {
+          res.send(err);
+      }
+  });
+})
+app.post("/kitchen", upload, function(req, res) {
+
+  const kit = new kitchen({
+    name: req.body.namek,
+    email: req.body.emailk,
+    phone: req.body.phonek,
+    productName: req.body.productNamek,
+    productPrice: req.body.productPricek,
+    image: req.file.filename
+  });
+
+  kit.save(function(err, foundItems) {
+    if (!err) {
+      res.redirect("/"); // Redirect to the home page
+    } else {
+      res.send(err);
+    }
+  });
+});
+app.post("/phone", upload, function(req, res) {
+
+  const pho = new phone({
+    name: req.body.namep,
+    email: req.body.emailp,
+    phone: req.body.phonep,
+    productName: req.body.productNamep,
+    productPrice: req.body.productPricep,
+    image: req.file.filename
+  });
+
+  pho.save(function(err, foundItems) {
+    if (!err) {
+      res.redirect("/"); // Redirect to the home page
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+
+
+
+
+
+app.post("/login", function(req,res){
+  const Username=req.body.username;
+  const Password=md5(req.body.password);
+  if(Username=="admin@gmail.com" && req.body.password=="123"){
+    res.render("select");
+  }else{
+    // res.send("err");
+  
+    user.findOne({username: Username}, function(err, foundItems) {
+      console.log(foundItems);
+      if(err) {
+        res.send("err");
+      } else {
+        console.log(foundItems);
+        if(foundItems) {
+          console.log(foundItems);
+          if(foundItems.password === Password) {
+            res.render("select");
+          } else {
+            res.send("err");
+          }
+        } else {
+          console.log(foundItems);
+          res.send("err33");
+        }
+      }
+    });
+    
+    }});
+
+
+
+    app.post("/skill", upload, function(req, res) {
+      const skillPerson = new skill({
+        name: req.body.name,
+        profession:req.body.profession,
+        experience:req.body.experience,
+        email: req.body.email,
+        phone:req.body.phone,
+        price:req.body.price
+      })
+      skillPerson.save(function(err, foundItems) {
+          if (!err) {
+              res.redirect("/"); // Redirect to the home page
+          } else {
+              res.send(err);
+          }
+      });
+  });
+    
+
+        
+  // app.post("/search", function(req, res) {
+  //   const searchTerm = req.body.search1;
+  
+  //   // Perform a MongoDB query to find items that match the search term
+  //   file.find({ productName: { $regex: searchTerm, $options: 'i' } }, function(err, foundItems) {
+  //     kitchen.find({ productName: { $regex: searchTerm, $options: 'i' } }, function(err, foundItemsk) {
+  //       phone.find({ productName: { $regex: searchTerm, $options: 'i' } }, function(err, foundItemsp) {
+  //     if (err) {
+  //       console.log(err);
+  //       res.send("Error occurred during the search.");
+  //     } else {
+  //       res.render("searchResults", { results: foundItems, recordss: foundItems, kitchen:foundItemsk, phone:foundItemsp }); // Pass the 'recordss' variable
+  //     }
+  //   })
+  // })
+  //   });
+  // });
+  app.post("/search", function(req, res) {
+    const searchTerm = req.body.search1;
+  
+    // Create an array to store the results from different collections
+    let searchResults = { recordss: [], kitchen: [], phone: [] };
+  
+    // Define a function to handle rendering the results
+    const renderResults = () => {
+      res.render("searchResults", searchResults);
+    };
+  
+    // Perform a MongoDB query to find items that match the search term in each collection
+    file.find({ productName: { $regex: searchTerm, $options: 'i' } }, function(err, foundItems) {
+      if (err) {
+        console.log(err);
+        return res.send("Error occurred during the search.");
+      }
+      searchResults.recordss = foundItems;
+      // Check if all queries are complete before rendering the results
+      if (Object.keys(searchResults).every((key) => searchResults[key].length !== 0)) {
+        renderResults();
+      }
+    });
+  
+    kitchen.find({ productName: { $regex: searchTerm, $options: 'i' } }, function(err, foundItemsk) {
+      if (err) {
+        console.log(err);
+        return res.send("Error occurred during the search.");
+      }
+      searchResults.kitchen = foundItemsk;
+      // Check if all queries are complete before rendering the results
+      if (Object.keys(searchResults).every((key) => searchResults[key].length !== 0)) {
+        renderResults();
+      }
+    });
+  
+    phone.find({ productName: { $regex: searchTerm, $options: 'i' } }, function(err, foundItemsp) {
+      if (err) {
+        console.log(err);
+        return res.send("Error occurred during the search.");
+      }
+      searchResults.phone = foundItemsp;
+      // Check if all queries are complete before rendering the results
+      if (Object.keys(searchResults).every((key) => searchResults[key].length !== 0)) {
+        renderResults();
+      }
+    });
+  });
+  
+  
+  
+
+
+
+
+
+app.listen(4000, function() {
+  console.log("Server started on port 3000.");
+});
